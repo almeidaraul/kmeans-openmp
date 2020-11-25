c83f29b7c71b35090cc77e8df72c2d6b0e3b374f
Raul Almeida <haltsimog@gmail.com>
2020-11-25 19:44:33 -0300

    tests ready

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 INPUTDIR="inputs"
 INPUTFILES=( "10x1M.txt" "10x2M.txt" "10x5M.txt" )
-CPUVALUES=( 1 2 4 8 )
+CPUVALUES=( 1 2 4 8 16 )
 CODE="$1.c"
 OUTPUTFILE="$2.csv"
-CFLAGS="-fopenmp"
-NUM_EXECS=5
+CFLAGS="-fopenmp -Wextra -O3"
+NUM_EXECS=10
 
 echo "Creating $OUTPUTFILE and compiling $CODE, skmeans.c"
 
@@ -17,8 +17,8 @@ echo "Creating reference output files"
 for (( INPUT=0; INPUT<${#INPUTFILES[@]}; INPUT++ ))
 do
 		INPUTFILE=$INPUTDIR"/"${INPUTFILES[INPUT]}
-		./oskmeans < $INPUTFILE > oskmeans-$INPUT.out
-		head oskmeans-$INPUT.out --lines=-2 > oskmeans-$INPUT.out
+		./oskmeans < $INPUTFILE > oskmeans.out
+		head oskmeans.out --lines=-2 > oskmeans-$INPUT.out
 done
 
 echo "Running parallel code"
@@ -36,8 +36,8 @@ do
 			./tmprun < $INPUTFILE > tmprun.out
 			EXECTIME=$(tail -n 1 tmprun.out)
 
-			head tmprun.out --lines=-2 > tmprun.out
-			diff tmprun.out oskmeans-$INPUT.out > difffile
+			head tmprun.out --lines=-2 > tmprun2.out
+			diff tmprun2.out oskmeans-$INPUT.out > difffile
 
 			OUTOK="y"
 			if [ -s "difffile" ]
@@ -46,7 +46,6 @@ do
 			fi
 			echo $CPU";"${INPUTFILES[INPUT]}";"$EXECTIME";"$OUTOK >> $OUTPUTFILE
 		done
-		rm oskmeans-$INPUT.out
 	done
 done
 rm tmprun oskmeans *.out difffile
